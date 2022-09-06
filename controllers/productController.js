3dfeb48e7759941351f7c8ee2e9f2e78f5700957
Caro <carito19@gmail.com>
2022-09-05 21:40:52 -0400

    Update controllers for methods put and delete Products

--- controllers/productController.js
+++ controllers/productController.js
@@ -20,7 +20,7 @@ exports.addProduct = (req, res) => {
     fs.readFileSync(`${__dirname}/../data/products.json`)
   );
   products.push(req.body);
-  fs.writeFileSync(`${__dirname}/data/products.json`, JSON.stringify(products));
+  fs.writeFileSync(`${__dirname}/../data/products.json`, JSON.stringify(products));
 
   res.status(200).json({
     status: "success",
@@ -41,6 +41,56 @@ exports.getProductById = (req, res) => {
       status: "success",
       data: {
         product: foundProduct,
+      },
+    });
+  } else {
+    res.status(404).json({
+      status: "not found",
+    });
+  }
+};
+
+
+exports.putProduct = (req, res) => {
+  const products = JSON.parse(
+    fs.readFileSync(`${__dirname}/../data/products.json`)
+  );
+
+  const foundProduct = products.find((p) => p.id == req.params.id);
+  if (foundProduct) {
+    const id_product = foundProduct.id-1;
+    products[id_product].name = req.body.name;
+    products[id_product].price = req.body.price;
+    products[id_product].category = req.body.category;
+    fs.writeFileSync(`${__dirname}/../data/products.json`, JSON.stringify(products));
+    res.status(200).json({
+      status: "success",
+      data: {
+        product: products,
+      },
+    });
+  } else {
+    res.status(404).json({
+      status: "not found",
+    });
+  }
+};
+
+
+exports.deleteProduct = (req, res) => {
+  const products = JSON.parse(
+    fs.readFileSync(`${__dirname}/../data/products.json`)
+  );
+
+  const foundProduct = products.find((p) => p.id == req.params.id);
+  if (foundProduct) {
+    const id_product = foundProduct.id-1;
+    products.splice(id_product,1);
+    fs.writeFileSync(`${__dirname}/../data/products.json`, JSON.stringify(products));
+    res.status(200).json({
+      status: "success",
+      data: {
+        product: products,
       },
     });
   } else {
